eaf1e0c8958c23a36417667af7cdf3b0080786fe
alexey <alexey33h@mail.ru>
2025-01-09 18:57:46 +0300

    init

---
+++ src/main/java/com/smart/home/fridgemanager/FridgemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.smart.home.fridgemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgemanagerApplication.class, args);
+    }
+
+}
